8447db80a08b9d6798cc83c58abf0a21b565692b
lix <lix@DESKTOP-JHAVF1U>
2023-03-16 16:40:49 +0800

    增加柱状图显示SQL

--- ISIA/UI/ISIA.UI.ANALYSIS/FrmSQLFullTextQueryAnalysis.Designer.cs
+++ ISIA/UI/ISIA.UI.ANALYSIS/FrmSQLFullTextQueryAnalysis.Designer.cs
@@ -29,11 +29,39 @@ namespace ISIA.UI.ANALYSIS
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram2 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series4 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView4 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series5 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView5 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series6 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView6 = new DevExpress.XtraCharts.LineSeriesView();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
             this.xtraUserControl1.SuspendLayout();
             this.dpnlRight.SuspendLayout();
+            this.dpnlRight_Container.SuspendLayout();
             this.dpnlLeft.SuspendLayout();
             this.tPanelBottomBase.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PopMenuBase)).BeginInit();
             this.tPanelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView6)).BeginInit();
             this.SuspendLayout();
             // 
             // xtraUserControl1
@@ -69,6 +97,8 @@ namespace ISIA.UI.ANALYSIS
             // 
             // dpnlRight_Container
             // 
+            this.dpnlRight_Container.Controls.Add(this.panelControl2);
+            this.dpnlRight_Container.Controls.Add(this.panelControl1);
             this.dpnlRight_Container.Size = new System.Drawing.Size(905, 660);
             // 
             // dpnlLeft
@@ -97,6 +127,56 @@ namespace ISIA.UI.ANALYSIS
             // 
             this.tPanelMain.Size = new System.Drawing.Size(1161, 688);
             // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.memoEdit1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 560);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(905, 100);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.memoEdit1.Location = new System.Drawing.Point(2, 2);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(901, 96);
+            this.memoEdit1.TabIndex = 0;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.chartControl1);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl2.Location = new System.Drawing.Point(0, 0);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(905, 560);
+            this.panelControl2.TabIndex = 1;
+            // 
+            // chartControl1
+            // 
+            xyDiagram2.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram2.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram2;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(2, 2);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.RuntimeHitTesting = true;
+            series4.Name = "Series 1";
+            lineSeriesView4.LineMarkerOptions.Size = 8;
+            series4.View = lineSeriesView4;
+            series5.Name = "Series 2";
+            series5.View = lineSeriesView5;
+            series6.Name = "Series 3";
+            series6.View = lineSeriesView6;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series4,
+        series5,
+        series6};
+            this.chartControl1.Size = new System.Drawing.Size(901, 556);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chartControl1_MouseDown);
+            // 
             // FrmSQLFullTextQueryAnalysis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -104,16 +184,39 @@ namespace ISIA.UI.ANALYSIS
             this.ClientSize = new System.Drawing.Size(1161, 718);
             this.Name = "FrmSQLFullTextQueryAnalysis";
             this.Text = "FrmSQLFullTextQueryAnalysis";
+            this.Load += new System.EventHandler(this.FrmSQLFullTextQueryAnalysis_Load);
             this.xtraUserControl1.ResumeLayout(false);
             this.dpnlRight.ResumeLayout(false);
+            this.dpnlRight_Container.ResumeLayout(false);
             this.dpnlLeft.ResumeLayout(false);
             this.tPanelBottomBase.ResumeLayout(false);
             this.tPanelBottomBase.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PopMenuBase)).EndInit();
             this.tPanelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.Utils.ToolTipController toolTipController1;
     }
 }
