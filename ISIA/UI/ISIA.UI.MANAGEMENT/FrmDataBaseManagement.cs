b94dce16db529b38f015348e1e4e2526ac5fd0e2
lix <lix@DESKTOP-JHAVF1U>
2023-03-27 18:10:53 +0800

    去掉时间

--- ISIA/UI/ISIA.UI.MANAGEMENT/FrmDataBaseManagement.cs
+++ ISIA/UI/ISIA.UI.MANAGEMENT/FrmDataBaseManagement.cs
@@ -29,10 +29,10 @@ namespace ISIA.UI.MANAGEMENT
         {
             InitializeComponent();
             bs = new BizDataClient("ISIA.BIZ.MANAGEMENT.DLL", "ISIA.BIZ.MANAGEMENT.DataBaseManagement");
-            dateStart.DateTime = DateTime.Now.AddDays(-1);
-            dateEnd.DateTime = DateTime.Now;
-            dtStart.DateTime = DateTime.Now.AddDays(-1);
-            dtEnd.DateTime = DateTime.Now;
+            //dateStart.DateTime = DateTime.Now.AddDays(-1);
+            //dateEnd.DateTime = DateTime.Now;
+            //dtStart.DateTime = DateTime.Now.AddDays(-1);
+            //dtEnd.DateTime = DateTime.Now;
         }
 
         public DataSet LoadData()
