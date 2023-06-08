60163af5ffa36cdb8204365241ba949450ffe8c8
li.xiong <>
2023-06-08 11:18:16 +0800

    修改physical_write_block

--- ISIA/BIZ/ISIA.BIZ.TREND/WorkloadTrendChart.cs
+++ ISIA/BIZ/ISIA.BIZ.TREND/WorkloadTrendChart.cs
@@ -486,7 +486,7 @@ SELECT sm.dbid
                 }
                 else if (arguments.ParamNamesString== "PHYSICAL_WRITE_BYTES_DELTA")
                 {
-                    tmpSql.AppendFormat("select sql_id,ROUND(sum({0})/8192,0) {0},instance_number,b.module,b.action,b.parsing_schema_name from (", arguments.ParamNamesString);
+                    tmpSql.AppendFormat("select sql_id,ROUND(sum({0})/8192,0) physical_write_block,instance_number,b.module,b.action,b.parsing_schema_name from (", arguments.ParamNamesString);
                 }
                 else
                 {
@@ -535,9 +535,9 @@ SELECT sm.dbid
                     tmpSql.AppendFormat("SELECT to_date(d.workdate,'{0}') workdate,ROUND(sum(d.{1})/1000000,0) {1},d.sql_id,d.instance_number,d.module,d.action,d.parsing_schema_name  FROM ( ", arguments.PARADEF,arguments.ParamNamesString);
                     
                 }
-                else if (arguments.ParamNamesString == "PHYSICAL_WRITES_PSEC")
+                else if (arguments.ParamNamesString == "PHYSICAL_WRITE_BLOCK"|| arguments.ParamNamesString == "PHYSICAL_WRITE_BYTES_DELTA" )
                 {
-                    tmpSql.AppendFormat("SELECT to_date(d.workdate,'{0}') workdate,ROUND(sum(d.{1})/8196,6) {1},d.sql_id,d.instance_number,d.module,d.action,d.parsing_schema_name  FROM ( ", arguments.PARADEF, arguments.ParamNamesString);
+                    tmpSql.AppendFormat("SELECT to_date(d.workdate,'{0}') workdate,ROUND(sum(d.{1})/8192,6) {1},d.sql_id,d.instance_number,d.module,d.action,d.parsing_schema_name  FROM ( ", arguments.PARADEF, arguments.ParamNamesString);
                 }
                 else
                 {
