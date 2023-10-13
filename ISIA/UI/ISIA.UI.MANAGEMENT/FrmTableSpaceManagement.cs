55467d3d129546f3e84ce58de46e126f05e1e7d0
chen.huan@isetdashanghai.com <chen.huan@isetdashanghai.com>
2023-10-13 14:08:29 +0800

    tablespqce 修改

--- ISIA/UI/ISIA.UI.MANAGEMENT/FrmTableSpaceManagement.cs
+++ ISIA/UI/ISIA.UI.MANAGEMENT/FrmTableSpaceManagement.cs
@@ -39,10 +39,7 @@ namespace ISIA.UI.MANAGEMENT
         {
 
             args = new DataBaseManagementArgsPack();
-            args.CATEGORY = txtMainCategory.Text;
-            args.CUSTOM01 = txtMainCustom.Text;
-            args.NAME = txtMainName.Text;
-            args.SUBCATEGORY = txtMainSub.Text;
+            
             ds = bs.ExecuteDataSet("GetDB", args.getPack());
 
             return ds;
@@ -61,8 +58,7 @@ namespace ISIA.UI.MANAGEMENT
         {
 
 
-            gridControl1.DataSource = ds.Tables[0];
-            gridView1.BestFitColumns();
+            
         }
 
         private void btnSelect_Click(object sender, EventArgs e)
@@ -78,40 +74,7 @@ namespace ISIA.UI.MANAGEMENT
             }
         }
 
-        private void btnExport_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (gridControl1.DataSource != null)
-                {
-                    SaveFileDialog fileDialog = new SaveFileDialog();
-                    fileDialog.Title = "Excel";
-                    fileDialog.Filter = "Excel(*.xls)|*.xls";
-                    DialogResult dialogResult = fileDialog.ShowDialog();
-                    if (dialogResult == DialogResult.OK)
-                    {
-                        gridView1.OptionsPrint.AutoWidth = false;
-                        try
-                        {
-                            gridView1.ExportToXls(fileDialog.FileName);
-
-                        }
-                        catch
-                        {
-                            TAP.UI.TAPMsgBox.Instance.ShowMessage(this.Text, EnumMsgType.INFORMATION, "  Excel is opening.");
-                        }
-                    }
-                }
-                else
-                {
-                    TAP.UI.TAPMsgBox.Instance.ShowMessage(this.Text, EnumMsgType.INFORMATION, "  Export Failed.");
-                }
-            }
-            catch (System.Exception ex)
-            {
-                TAP.UI.TAPMsgBox.Instance.ShowMessage(this.Text, EnumMsgType.ERROR, ex.ToString());
-            }
-        }
+       
 
         private void txtSave_Click(object sender, EventArgs e)
         {
@@ -160,72 +123,11 @@ namespace ISIA.UI.MANAGEMENT
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle) as DataRow;
-            if (dr!=null)
-            {
-                tabPane1.SelectedPage = tabNavigationPage2;
-                txtCATEGORY.Text = dr["CATEGORY"].ToString();
-                txtCustome.Text = dr["CUSTOM01"].ToString();
-                txtDESCRIPTION.Text= dr["DESCRIPTION"].ToString();
-                txtName.Text= dr["NAME"].ToString();
-                txtSEQUENCES.Text = dr["SEQUENCES"].ToString();
-                txtSUBCATEGORY.Text = dr["SUBCATEGORY"].ToString();
-                if (dr["USED"].ToString()=="YES")
-                {
-                    rdoUsed.SelectedIndex = 0;
-                }
-                else
-                {
-                    rdoUsed.SelectedIndex = 1;
-                }
-                if (dr["ISALIVE"].ToString() == "YES")
-                {
-                    rdoIsalive.SelectedIndex = 0;
-                }
-                else
-                {
-                    rdoIsalive.SelectedIndex = 1;
-                }
-
-            }
+            
         }
 
-        private void btnDel_Click(object sender, EventArgs e)
-        {
-            int[] i = gridView1.GetSelectedRows();
-            if (i==null&&i.Count()<1)
-            {
-                return;
-            }
-            string tmpMessage = _translator.ConvertGeneralTemplate(EnumVerbs.UPDATE, EnumGeneralTemplateType.CONFIRM, "");
-            DialogResult dialog = TAP.UI.TAPMsgBox.Instance.ShowMessage(Text, EnumMsgType.CONFIRM, tmpMessage);
-            if (dialog.ToString() == "Yes")
-            {
-                foreach (var item in i)
-                {
-                    DataRow dr = gridView1.GetDataRow(i[0]) as DataRow;
-                    if (dr != null)
-                    {
-                        args = new DataBaseManagementArgsPack();
-                        args.ROWID = dr["ROWID"].ToString();
-                        bs.ExecuteModify("DelteTCODE", args.getPack());
-                    }
-
-                }
-                TAP.UI.TAPMsgBox.Instance.ShowMessage(Text, EnumMsgType.WARNING, "Success.");
-                btnSelect_Click(null, null);
-            }
-        }
-
-        private void gridView1_DoubleClick(object sender, EventArgs e)
-        {
-            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle) as DataRow;
-            if (dr != null)
-            {
-                btnUpdate_Click(null, null);
-            }
-        }
-
+      
+       
         private void txtCATEGORY_TextChanged(object sender, EventArgs e)
         {
             SetText(sender as TextEdit);
